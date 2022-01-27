da41ca798aba56cb068f599890c8bd413fbec6d8
Tino Didriksen <mail@tinodidriksen.com>
2022-01-27 16:06:57 +0000

    Re-arrange files and rename vendor; Add dumper

---
+++ _inc/dump.php
@@ -0,0 +1,76 @@
+#!/usr/bin/env php
+<?php
+declare(strict_types=1);
+require_once __DIR__.'/lib.php';
+
+if (!empty($_SERVER['REMOTE_ADDR'])) {
+	die("No remote use!");
+}
+
+$db = db();
+
+$since = strtotime($argv[1] ?? '1970-01-01 00:00:00');
+echo "Since: $since\n";
+
+$trs = [];
+$pairs = [];
+$stm = $db->prepexec("SELECT * FROM translations WHERE t_ctime >= ? ORDER BY t_id ASC", [$since]);
+while ($row = $stm->fetch()) {
+	$row['t_result'] = json_decode($row['t_result'], true);
+	$trs[$row['t_id']] = $row;
+	$pairs[$row['t_pair']][$row['t_id']] = $row;
+}
+
+$first = date('Ymd', intval(current($trs)['t_ctime']));
+
+foreach ($pairs as $p => $ts) {
+	$fh = fopen("trx-$p-$first.csv", 'wb');
+	$fields = "ID,Date,Input";
+	if (array_key_exists('o', current($ts)['t_result'])) {
+		$fields .= ",Output";
+	}
+	if (array_key_exists('g', current($ts)['t_result'])) {
+		$fields .= ",Glossed";
+	}
+	if (array_key_exists('m', current($ts)['t_result'])) {
+		$fields .= ",Moved";
+	}
+	$fields .= "\n";
+	fwrite($fh, $fields);
+	foreach ($ts as $t) {
+		$t['t_ctime'] = date('Y-m-d H:i:s', intval($t['t_ctime']));
+		$fields = [$t['t_id'], $t['t_ctime'], normalize_text($t['t_result']['i'])];
+		if (array_key_exists('o', $t['t_result'])) {
+			$fields[] = normalize_text($t['t_result']['o']);
+		}
+		if (array_key_exists('g', $t['t_result'])) {
+			$fields[] = normalize_text($t['t_result']['g']);
+		}
+		if (array_key_exists('m', $t['t_result'])) {
+			$fields[] = normalize_text($t['t_result']['m']);
+		}
+		fputcsv($fh, $fields);
+	}
+	fclose($fh);
+}
+
+$fh = fopen("feedback-$first.txt", 'wb');
+$stm = $db->prepexec("SELECT * FROM feedback WHERE f_ctime >= ? ORDER BY f_id ASC", [$since]);
+while ($row = $stm->fetch()) {
+	$out = "";
+	$out .= "ID: {$row['f_id']}\n";
+	$out .= "Translation ID: {$row['t_id']}\n";
+	$out .= "Translation input: ".str_replace("\n", ' \n ', normalize_text($trs[$row['t_id']]['t_result']['i']))."\n";
+	$out .= "Feedback for: ".implode(" ; ", $trs[$row['t_id']]['t_result']['e'][$row['f_which']])."\n";
+	if (!empty($row['f_email'])) {
+		$out .= "Feedback from: {$row['f_email']}\n";
+	}
+	$out .= "Feedback: {$row['f_comment']}\n";
+	$out .= "\n================\n";
+	fwrite($fh, $out);
+}
+fclose($fh);
+
+if (filesize("feedback-$first.txt") === 0) {
+	unlink("feedback-$first.txt");
+}
---
+++ _inc/lib.php
@@ -0,0 +1,120 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__.'/../_vendor/autoload.php';
+
+$GLOBALS['-db'] = null;
+
+function db(): object {
+	if ($GLOBALS['-db']) {
+		return $GLOBALS['-db'];
+	}
+
+	$db_file = __DIR__.'/nutserut.sqlite';
+	$existed = file_exists($db_file) && (filesize($db_file) > 4096);
+
+	$db = new \TDC\PDO\SQLite($db_file);
+
+	$db->exec("PRAGMA auto_vacuum = INCREMENTAL");
+	$db->exec("PRAGMA case_sensitive_like = ON");
+	$db->exec("PRAGMA foreign_keys = ON");
+	$db->exec("PRAGMA journal_mode = WAL");
+	$db->exec("PRAGMA locking_mode = EXCLUSIVE");
+	$db->exec("PRAGMA synchronous = NORMAL");
+	$db->exec("PRAGMA threads = 4");
+	$db->exec("PRAGMA trusted_schema = OFF");
+
+	if (!$existed) {
+		$db->exec(file_get_contents(__DIR__.'/schema.sql'));
+	}
+
+	$GLOBALS['-db'] = $db;
+	return $db;
+}
+
+function normalize_text($txt) {
+	$txt = str_replace("\r\n", "\n", $txt);
+	$txt = str_replace("\r", '', $txt);
+	$txt = preg_replace('~</?s[^>]*>~s', '', $txt);
+	$txt = preg_replace('~\n[ \t]+~s', "\n", $txt);
+	$txt = preg_replace('~[ \t]+\n~s', "\n", $txt);
+	$txt = preg_replace('~[ \t][ \t]+~s', ' ', $txt);
+	$txt = preg_replace('~\n\n\n+~s', "\n\n", $txt);
+	$txt = trim($txt);
+	if (empty($txt)) {
+		return '';
+	}
+
+	if (mb_strlen($txt) > 500) {
+		$txt = mb_substr($txt, 0, 500);
+		$txt = preg_replace('~\S+\s*$~us', '', $txt);
+		$txt .= ' …';
+	}
+
+	return $txt;
+}
+
+function handle_via_port($input, $port) {
+	global $rv;
+
+	$nonce = mt_rand();
+	$nonced = "<s-$nonce>\n".$input."\n</s-$nonce>";
+
+	$s = fsockopen('localhost', $port, $errno, $errstr, 1);
+	if ($s === false) {
+		header('X-Nutserut-Error: fsockopen '.$errno, false);
+		$rv['errors'][] = 'fsockopen';
+		return '';
+	}
+	if (fwrite($s, $nonced."\n<END-OF-INPUT>\n") === false) {
+		header('X-Nutserut-Error: fwrite', false);
+		$rv['errors'][] = 'fwrite';
+		return '';
+	}
+	$output = stream_get_contents($s);
+	if (!preg_match('~<s-'.$nonce.'>\n~', $output)) {
+		header('X-Nutserut-Error: nonce', false);
+		$rv['errors'][] = 'nonce';
+		$output = '';
+	}
+
+	$output = preg_replace('~</?s[^>]*>~s', '', $output);
+	$output = trim($output);
+	return $output;
+}
+
+function b64_slug($rv) {
+	$rv = base64_encode($rv);
+	$rv = trim($rv, '=');
+	$rv = str_replace('+', 'z', $rv);
+	$rv = str_replace('/', 'Z', $rv);
+	$rv = preg_replace('~^\d~', 'n', $rv);
+	return $rv;
+}
+
+function json_encode_vb($v, $o=0) {
+	return json_encode($v, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | $o);
+}
+
+function save_translation($hash, $pair, $result) {
+	$db = db();
+	$db->beginTransaction();
+	$upd = $db->prepare("UPDATE translations SET t_atime = strftime('%s', 'now'), t_hits = t_hits + 1 WHERE t_hash = ?");
+	$upd->execute([$hash]);
+	if ($upd->rowCount() == 0) {
+		$db->prepexec("INSERT INTO translations (t_hash, t_pair, t_result) VALUES (?, ?, ?)", [$hash, $pair, json_encode_vb($result)]);
+	}
+	$db->commit();
+}
+
+function load_translation($hash) {
+	$db = db();
+	$rv = $db->prepexec("SELECT t_result FROM translations WHERE t_hash = ?", [$hash])->fetchAll();
+	if (!empty($rv)) {
+		return json_decode($rv[0]['t_result'], true);
+	}
+	return false;
+}
+
+function hashify(&$rv) {
+	$rv['hash'] = strtolower(substr(b64_slug(hash('sha256', "{$rv['action']}:{$rv['input']}", true)), 0, 40));
+}
