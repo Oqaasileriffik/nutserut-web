09458bcc4dac0287e990e21af279f09fb7d2d349
Tino Didriksen <mail@tinodidriksen.com>
2023-03-03 12:56:01 +0000

    Add Google Analytics 4 and Matomo trackers, remove old tracker

--- index.php
+++ index.php
@@ -275,14 +275,26 @@
 	</div>
 </footer>
 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-N1BDG3Y82F"></script>
 <script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-	ga('create', 'UA-87771-26', 'auto');
-	ga('send', 'pageview');
+  gtag('config', 'G-N1BDG3Y82F');
+</script>
+
+<script>
+  var _paq = window._paq = window._paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//oqaasileriffik.gl/matomo/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '3']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
 </script>
 
 </body>
